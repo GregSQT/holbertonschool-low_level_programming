c4bbea2f3149bc2443d044563af703b21562d5ec
GregSQT <9604@holbertonstudents.com>
2024-07-22 13:23:58 +0000

    message

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/* Short description */
+
+/**
+ * puts.c - This is a function description
+ * @s: First parameter
+ *
+ * Return: void
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
+/**
+ * rev_string - Description
+ * @str: First parameter
+ *
+ * return: void
+ */
+void rev_string(char *str)
+{
+	int length = _strlen(str);
+	int i, j;
+
+	for (i = 0, j = length - 1; i < j; i++, j--)
+	{
+		char c = str[i];
+		str[i] = str[j];
+		str[j] = c;
+	}
+}
