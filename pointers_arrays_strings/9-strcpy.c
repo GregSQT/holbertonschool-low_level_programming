829c3c0602a5a7edea113884490cb8ee34cc9a42
GregSQT <9604@holbertonstudents.com>
2024-07-22 13:31:20 +0000

    message

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/* Short description */
+
+/**
+  * print_array - Function
+ * @s: Parameter 1
+ *
+ * Return: void
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
+/**
+ * _strcpy - Description
+ * @dest: First parameter
+ * @src: First parameter
+ *
+ * return: a char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int length = _strlen(src);
+	int i;
+
+	for (i = 0; i < length; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return dest;
+}
