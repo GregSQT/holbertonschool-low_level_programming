17b7e909d093bf0d607e4587b18c960cc3d11a99
GregSQT <9604@holbertonstudents.com>
2024-07-22 13:30:37 +0000

    message

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+/* Short description */
+
+/**
+ * print_array - Function
+ * @a: Parameter 1
+ * @n: Parameter 2
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
