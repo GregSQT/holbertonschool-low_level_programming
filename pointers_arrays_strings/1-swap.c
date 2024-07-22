6b5d3bd51087ea8a31e59438dfd20db128564f11
GregSQT <9604@holbertonstudents.com>
2024-07-22 13:07:40 +0000

    message

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
+/* Short description */
 
 /**
- * Exervices en C 
+ * swap_int - Function
+ * @a: parameter 1
+ * @b: parameter 2
  *
+ * Return: Void
  */
-void swap_int(int *a, int *b);
+void swap_int(int *a, int *b)
 {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+	*a = *a + *b;
+	*b = *a - *b;
+	*a = *a - *b;
 }
