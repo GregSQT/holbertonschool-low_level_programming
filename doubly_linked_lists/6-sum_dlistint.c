55c0a02ef12b837ceede19a11b78aa247fc1bd9b
GregSQT <9604@holbertonstudents.com>
2024-07-22 14:21:15 +0000

    message

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/* Short description */
+
+/**
+ * sum_dlistint - This is a function description
+ * @head: Parameter 1
+ * 
+ * Return: result
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int result = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (head != NULL)
+	{
+		result = result + head->n;
+		head = head->next;
+	}
+
+	return (result);
+}
